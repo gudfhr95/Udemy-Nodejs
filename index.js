293d1f9320d2cd5e8ed5b6ca9fdb0072b1b5f9ed
Hyungstler <ghldnjsdlf@gmail.com>
2022-04-21 12:49:15 +0900

    Reading Our Planets Data

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const reuslts = [];
+
+fs.createReadStream("kepler_data.csv")
+  .on("data", (data) => {
+    reuslts.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(reuslts);
+    console.log("done");
+  });
+// parse();
