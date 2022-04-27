474851da942b1b4c3d66b190abd224b08f60a09d
Hyungstler <ghldnjsdlf@gmail.com>
2022-04-27 15:40:06 +0900

    Connecting to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 
@@ -8,7 +9,19 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:Bq4Wwvs0g9Ssmc6S@nasacluster.1icr5.mongodb.net/nasa?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
