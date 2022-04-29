bc894ebcbb4b546a23e3a175337e6703e9310198
Hyungstler <ghldnjsdlf@gmail.com>
2022-04-29 14:43:05 +0900

    Restricting Access to Endpoints

--- server.js
+++ server.js
@@ -60,7 +60,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true; // TODO
+  console.log("Current user is:", req.user);
+  const isLoggedIn = req.isAuthenticated() && req.user; // TODO
   if (!isLoggedIn) {
     return res.status(401).json({
       error: "You must log in!",
